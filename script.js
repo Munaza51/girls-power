4d172acc3d62e184890a91bfc4d09e1b8732b4e2
munaza hazrati <63694714+Munaza51@users.noreply.github.com>
2024-03-20 23:18:21 +0430

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('btn');
+const text = document.getElementById('text');
+
+button.addEventListener('click', () => {
+    text.textContent = 'Girls are strong, smart, and powerful!';
+});
